70434839524b4d41f159a5a0618fd388cc6edc55
Shakib <shakibjadside01@gmail.com>
2022-03-23 06:35:51 -0400

    Added Navigation Bar

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,7 @@ const Header = ({ siteTitle }) => (
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.05rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -27,6 +27,21 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+    </div>
+    <div class = "nav-bar"
+      style={{
+        margin: '0 auto',
+        maxWidth: 980,
+      }}
+    >
+      <ul class = "nav-links">
+          <h3>
+            <a href="/"><li>Home</li></a></h3>
+          <h3>
+            <a href="/page-2"><li>Page 2</li></a></h3>
+          <h3>
+            <a href="/using-typescript"><li>Using TypeScript</li></a></h3>
+      </ul>
     </div>
   </header>
 )
